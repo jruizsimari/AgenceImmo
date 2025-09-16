8fa092bdfcd539eb849760505c918e7d2bbff874
Julio Ruiz Simari <jruizsimari@gmail.com>
2025-09-16 11:30:59 +0100

    Add softdelete on property, and return '' on get cast password

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $properties = Property::available(true)->recent()->limit(4)->get();
+        $properties = Property::available()->recent()->limit(4)->get();
+
+        $user = User::first();
+        $user->password = '0000';
+        dd($user->password, $user);
+
         return view('home', [
             'properties' => $properties
         ]);
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Property extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title',
@@ -33,5 +36,14 @@ class Property extends Model
     public function getSlug()
     {
         return Str::slug($this->title);
+    }
+    public function scopeAvailable(Builder $builder, bool $available = true): Builder
+    {
+        return $builder->where('sold', !$available);
+    }
+
+    public function scopeRecent(Builder $builder): Builder
+    {
+        return $builder->orderBy('created_at', 'desc');
     }
 }
