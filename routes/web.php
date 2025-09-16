926bc1a980e014f6599a435b63a69d39f2238ae6
Julio Ruiz Simari <jruizsimari@gmail.com>
2025-09-16 19:33:07 +0100

    Laravel Breeze installation and config

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\Admin\OptionController;
 use App\Http\Controllers\Admin\PropertyController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,19 +30,20 @@ Route::post('/contact/{property}', [\App\Http\Controllers\PropertyController::cl
     'property' => $idRegex,
 ]);
 
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(function () {
     Route::resource('property', PropertyController::class)->except(['show']);
     Route::resource('option', OptionController::class)->except(['show']);
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/login', [AuthController::class, 'login'])
-    ->middleware('guest')
-    ->name('login');
-Route::post('/login', [AuthController::class, 'doLogin']);
-Route::delete('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth')
-    ->name('logout');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 
-
+require __DIR__.'/auth.php';
