503ab3f75321827b3c88a319dd4bfd51657afa9d
Julio Ruiz Simari <jruizsimari@gmail.com>
2025-09-19 18:25:31 +0100

    Add Cache Repository as DI in the Weather service and change test

--- app/Weather.php
+++ app/Weather.php
@@ -2,13 +2,19 @@
 
 namespace App;
 
+use Illuminate\Cache\Repository;
 use Illuminate\Support\Facades\Cache;
 
 class Weather
 {
+    public function __construct(private Repository $cache)
+    {
+
+    }
+
     public function isSunnyTomorrow()
     {
-        $result = Cache::get('weather');
+        $result = $this->cache->get('weather');
         if ($result !== null) {
             return $result;
         }
--- tests/Unit/WeatherTest.php
+++ tests/Unit/WeatherTest.php
@@ -3,44 +3,37 @@
 namespace Tests\Unit;
 
 use App\Weather;
+use Illuminate\Cache\Repository;
 use Illuminate\Support\Facades\Cache;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class WeatherTest extends TestCase
 {
-    // C’est utilisé pour éviter la contamination entre tests due à la réutilisation de l’instance de Cache par la facade.
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        Cache::clearResolvedInstances();
-    }
-
     /**
      * A basic unit test example.
      */
     public function test_isSunnyTomorrow_function_returns_false(): void
     {
-        // Les facades ne sont pas reconnus dans les tests unitaires
-        // Solution : mocker les façades à la volée
-        Cache::shouldReceive('get')
+        $mock = Mockery::mock(Repository::class);
+        $mock->shouldReceive('get')
             ->with('weather')
             ->once()
             ->andReturn(false);
 
-        $weather = new Weather();
+        $weather = new Weather($mock);
         $this->assertFalse($weather->isSunnyTomorrow());
     }
 
     public function test_isSunnyTomorrow_function_returns_true(): void
     {
-        // Les facades ne sont pas reconnus dans les tests unitaires
-        // Solution : mocker les façades à la volée
-        Cache::shouldReceive('get')
+        $mock = Mockery::mock(Repository::class);
+        $mock->shouldReceive('get')
             ->with('weather')
             ->once()
             ->andReturn(null);
 
-        $weather = new Weather();
+        $weather = new Weather($mock);
         $this->assertTrue($weather->isSunnyTomorrow());
     }
 }
